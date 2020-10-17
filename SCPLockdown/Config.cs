2067cbba72c140403fbf6c41b87f73cf6aff923a
almightylks <lukasbarber@web.de>
2020-10-17 03:42:28 +0200

    Adjusted configs

--- SCPLockdown/Config.cs
+++ SCPLockdown/Config.cs
@@ -6,9 +6,17 @@ namespace ScpLockdown
 {
     public sealed class Config : IConfig
     {
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled { get; set; } = true;
 
-        [Description("The affected SCPs and their duration of lockdown.")]
-        public Dictionary<RoleType, int> AffectedScps { get; set; } = new Dictionary<RoleType, int>();
+        [Description("The affected SCPs and their duration [seconds] of lockdown.")]
+        public Dictionary<RoleType, int> AffectedScps { get; set; } = new Dictionary<RoleType, int>()
+        {
+            { RoleType.Scp173,60 },
+            { RoleType.Scp096,60 },
+            { RoleType.Scp106,60 },
+            { RoleType.Scp049,60 },
+            { RoleType.Scp93989,60 },
+            { RoleType.Scp93953,60 }
+        };
     }
 }
